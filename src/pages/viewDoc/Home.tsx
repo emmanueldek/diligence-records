982052d8854f9d88dbd99f6166ab5b3640dc9834
emmanueldek <eowolabi@noemdek.co.uk>
2024-02-21 15:19:11 +0100

    added pdf renderer

--- src/pages/viewDoc/Home.tsx
+++ src/pages/viewDoc/Home.tsx
@@ -3,13 +3,13 @@ import { Worker, Viewer } from "@react-pdf-viewer/core";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 
 type ViewerProps = {
-  fileUrl: string;
+  fileUrl?: string;
 };
 
 const Home: React.FC<ViewerProps> = ({ fileUrl }) => {
   return (
     <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-      <Viewer fileUrl={fileUrl} />
+      <Viewer fileUrl={fileUrl ? fileUrl : ""} />
     </Worker>
   );
 };

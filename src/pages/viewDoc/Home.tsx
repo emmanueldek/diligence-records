324f5fdb1fe45aaa3053a87f912fe4b48be4c9bc
emmanueldek <eowolabi@noemdek.co.uk>
2024-02-21 17:16:36 +0100

    changes pdf renderer

--- src/pages/viewDoc/Home.tsx
+++ src/pages/viewDoc/Home.tsx
@@ -1,27 +1,122 @@
+// import React from "react";
+// import { Worker, Viewer } from "@react-pdf-viewer/core";
+// import "@react-pdf-viewer/core/lib/styles/index.css";
+// import { getAuthToken } from "@/helpers/authTokens";
+
+// type ViewerProps = {
+//   fileUrl?: string;
+// };
+
+// const Home: React.FC<ViewerProps> = ({ fileUrl }) => {
+//   const token = getAuthToken();
+
+//   return (
+//     <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+//       <Viewer
+//         fileUrl={fileUrl ? fileUrl : ""}
+//         httpHeaders={{
+//           "Access-Control-Allow-Origin": "*",
+//           SPACES_SECRET_KEY: "F5nTpzfGyEuMny89fkH2ouXp2vnc1ZCaIN0HLewzwig",
+//           Authorization: `Bearer ${token}`,
+//         }}
+//         withCredentials={true}
+//       />
+//     </Worker>
+//   );
+// };
+
+// export default Home;
+
+// import React from "react";
+// import { pdfjs } from "react-pdf";
+// import { Document, Page } from "react-pdf";
+// import { useState } from "react";
+
+// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+//   "pdfjs-dist/build/pdf.worker.min.js",
+//   import.meta.url,
+// ).toString();
+
+// type ViewerProps = {
+//   fileUrl?: string;
+// };
+
+// const Home: React.FC<ViewerProps> = ({ fileUrl }) => {
+//   const [numPages, setNumPages] = useState<number>();
+//   const [pageNumber, setPageNumber] = useState<number>(1);
+
+//   function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+//     setNumPages(numPages);
+//   }
+//   return (
+//     <div>
+//       {" "}
+//       <Document file={fileUrl} onLoadSuccess={onDocumentLoadSuccess}>
+//         <Page pageNumber={pageNumber} />
+//       </Document>
+//       <p>
+//         Page {pageNumber} of {numPages}
+//       </p>
+//     </div>
+//   );
+// };
+
+// export default Home;
+
 import React from "react";
-import { Worker, Viewer } from "@react-pdf-viewer/core";
-import "@react-pdf-viewer/core/lib/styles/index.css";
-import { getAuthToken } from "@/helpers/authTokens";
+import "../../assets/css/index.css";
+import {
+  PdfViewerComponent,
+  Toolbar,
+  Magnification,
+  Navigation,
+  LinkAnnotation,
+  BookmarkView,
+  ThumbnailView,
+  Print,
+  TextSelection,
+  Annotation,
+  TextSearch,
+  FormFields,
+  FormDesigner,
+  Inject,
+} from "@syncfusion/ej2-react-pdfviewer";
 
 type ViewerProps = {
   fileUrl?: string;
 };
 
 const Home: React.FC<ViewerProps> = ({ fileUrl }) => {
-  const token = getAuthToken();
-
   return (
-    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-      <Viewer
-        fileUrl={fileUrl ? fileUrl : ""}
-        httpHeaders={{
-          "Access-Control-Allow-Origin": "*",
-          SPACES_SECRET_KEY: "F5nTpzfGyEuMny89fkH2ouXp2vnc1ZCaIN0HLewzwig",
-          Authorization: `Bearer ${token}`,
-        }}
-        withCredentials={true}
-      />
-    </Worker>
+    <div>
+      {" "}
+      <div className="control-section">
+        {/* Render the PDF Viewer */}
+        <PdfViewerComponent
+          id="container"
+          documentPath={fileUrl}
+          resourceUrl="https://cdn.syncfusion.com/ej2/23.1.40/dist/ej2-pdfviewer-lib"
+          style={{ height: "640px" }}
+        >
+          <Inject
+            services={[
+              Toolbar,
+              Magnification,
+              Navigation,
+              Annotation,
+              LinkAnnotation,
+              BookmarkView,
+              ThumbnailView,
+              Print,
+              TextSelection,
+              TextSearch,
+              FormFields,
+              FormDesigner,
+            ]}
+          />
+        </PdfViewerComponent>
+      </div>
+    </div>
   );
 };
 
